d9bb2ab0de00def05aa776896f9097c632cab2de
DavidG1000 <dk3212007@yahoo.com>
2021-11-15 21:15:24 +0100

    aangepast

--- cinema Bjorn 2.py
+++ cinema Bjorn 2.py
@@ -1,17 +1,28 @@
-leeftijd = 0
+leeftijd = 4
 prijs = 0
 film3d = "nee"
+lang = "nee"
+student = "j"
 
-
-if(leeftijd < 12):
-    prijs = 3
-elif(leeftijd < 17):
+if(leeftijd < 15):
     prijs = 4
-else:
+elif(leeftijd < 19):
     prijs = 5
-if(film3d = "ja"):
+elif(leeftijd < 22):
+    prijs = 6
+else:
+    prijs = 7
+if(film3d == "j"):
     prijs = prijs +2
-else
+else:
+    prijs = prijs + 0
+if(lang == "j"):
+    prijs = prijs + 2
+else:
+    prijs = prijs + 0
+if (student == "j") and (leeftijd < 26):
+    prijs = prijs - 2
+else:
     prijs = prijs + 0
 
-print("persoon 1 betaald:",kostprijs)
+print("Persoon 1 betaald:",prijs,"euro")
