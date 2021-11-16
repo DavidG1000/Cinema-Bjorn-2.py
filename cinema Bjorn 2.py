2d37f0564f7d038ad0953c439da0ad9eea68f2af
DavidG1000 <dk3212007@yahoo.com>
2021-11-16 10:45:44 +0100

    aangepast 2

--- cinema Bjorn 2.py
+++ cinema Bjorn 2.py
@@ -1,28 +1,50 @@
-leeftijd = 4
+import math
+aantal = int(input("Aantal personen ? "))
+persLid = input("Bent u personeelslid ? (j/n)")
+leeftijd = 0
 prijs = 0
-film3d = "nee"
-lang = "nee"
-student = "j"
+film3d = 0
+lang = 0
+student = 0
+totaalPrijs = 0
+korting = 0
 
-if(leeftijd < 15):
-    prijs = 4
-elif(leeftijd < 19):
-    prijs = 5
-elif(leeftijd < 22):
-    prijs = 6
-else:
-    prijs = 7
-if(film3d == "j"):
-    prijs = prijs +2
-else:
-    prijs = prijs + 0
-if(lang == "j"):
-    prijs = prijs + 2
-else:
-    prijs = prijs + 0
-if (student == "j") and (leeftijd < 26):
-    prijs = prijs - 2
-else:
-    prijs = prijs + 0
+for i in range(1,aantal+1):
+    print("Persoon ",i)
+    leeftijd = int(input("Geef de leeftijd in "))
+    student = input("Student ? (j/n) ")
+    film3d = input("Is de film in 3D ? (j/n) ")
+    lang = input("Is het een lang/speelfilm ? (j/n) ")
 
-print("Persoon 1 betaald:",prijs,"euro")
+    if(leeftijd < 15):
+        prijs = 4
+    elif(leeftijd < 19):
+        prijs = 5
+    elif(leeftijd < 22):
+        prijs = 6
+    else:
+        prijs = 7
+    if(film3d == "j"):
+        prijs = prijs +2
+    else:
+        prijs = prijs + 0
+    if(lang == "j"):
+        prijs = prijs + 2
+    else:
+        prijs = prijs + 0
+    if (student == "j") and (leeftijd < 26):
+        prijs = prijs - 2
+    else:
+        prijs = prijs + 0
+    totaalPrijs = totaalPrijs + prijs
+    print("Persoon",i, "betaald:",prijs,"euro")
+
+if (persLid == "j"):
+    korting=(totaalPrijs*10)/100
+    print("Het totaal is ",totaalPrijs-korting,"euro (met personeelskorting)")
+else: print(" Het totaal is", totaalPrijs,"euro" )
+
+
+
+
+
